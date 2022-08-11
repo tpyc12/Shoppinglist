51fbb3907b117825f43fab84d98f17359b22076b
tpyc12 <tpyc12@yandex.ru>
2022-08-11 22:35:19 +0300

    Create presentation layer. feat: GetShopListUseCase.kt feat: MainViewModel.kt feat: ShopListRepositoryImpl.kt init: shopItemList in ShopListRepositoryImpl.kt

--- app/src/main/java/com/myhome/android/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/myhome/android/shoppinglist/presentation/MainActivity.kt
@@ -10,13 +10,19 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
 
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this){
             Log.d("LOG", it.toString())
+            if (count == 0){
+                count++
+                val item = it[0]
+                viewModel.changeEnabledState(item)
+            }
         }
-        viewModel.getShopList()
     }
 }
