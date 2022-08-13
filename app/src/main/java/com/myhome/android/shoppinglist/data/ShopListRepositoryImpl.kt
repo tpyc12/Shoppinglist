183e59a667bb940361151d143b33c40fe09ac3c3
tpyc12 <tpyc12@yandex.ru>
2022-08-13 17:28:35 +0300

    feat: ShopListAdapter.kt

--- app/src/main/java/com/myhome/android/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/myhome/android/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -5,18 +5,19 @@ import androidx.lifecycle.MutableLiveData
 import com.myhome.android.shoppinglist.domain.ShopItem
 import com.myhome.android.shoppinglist.domain.ShopListRepository
 import java.lang.RuntimeException
+import kotlin.random.Random
 
 object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopListLD = MutableLiveData<List<ShopItem>>()
 
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({o1, o2 -> o1.id.compareTo(o2.id)})
 
     private var autoIncrementId = 0
 
     init {
         for (i in 0..10){
-            val item = ShopItem("name$i", i, true)
+            val item = ShopItem("name$i", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
---
+++ app/src/main/java/com/myhome/android/shoppinglist/presentation/ShopListAdapter.kt
@@ -0,0 +1,62 @@
+package com.myhome.android.shoppinglist.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.myhome.android.shoppinglist.R
+import com.myhome.android.shoppinglist.domain.ShopItem
+import java.lang.RuntimeException
+
+class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val layout = when (viewType) {
+            VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
+            VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
+            else -> throw RuntimeException("Unknown view type $viewType")
+        }
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
+        return ShopItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem = shopList[position]
+        with(holder) {
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            view.setOnLongClickListener {
+                true
+            }
+        }
+    }
+
+    override fun getItemCount() = shopList.size
+
+    override fun getItemViewType(position: Int): Int {
+        return if (shopList[position].enabled) {
+            VIEW_TYPE_ENABLED
+        } else {
+            VIEW_TYPE_DISABLED
+        }
+    }
+
+    inner class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+
+    companion object {
+        const val VIEW_TYPE_ENABLED = 100
+        const val VIEW_TYPE_DISABLED = 101
+
+        const val MAX_POOL_SIZE = 20
+    }
+}
