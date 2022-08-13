8cad4c0c833636df34ed654f3d759231371049e5
tpyc12 <tpyc12@yandex.ru>
2022-08-13 19:34:19 +0300

    feat: ShopListDiffCallback.kt fix: notifyDataSetChanged

--- app/src/main/java/com/myhome/android/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/myhome/android/shoppinglist/presentation/ShopListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.myhome.android.shoppinglist.R
 import com.myhome.android.shoppinglist.domain.ShopItem
@@ -19,8 +20,10 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
 
     var shopList = listOf<ShopItem>()
         set(value) {
+            val callback = ShopListDiffCallback(shopList, value)
+            val diffResult = DiffUtil.calculateDiff(callback)
+            diffResult.dispatchUpdatesTo(this)
             field = value
-            notifyDataSetChanged()
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
