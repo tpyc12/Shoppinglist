ec34566b3df7cd25cdab7b67252a13d930823879
tpyc12 <tpyc12@yandex.ru>
2022-08-14 13:16:52 +0300

    feat: ShopItemViewModel.kt

--- app/src/main/java/com/myhome/android/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/myhome/android/shoppinglist/presentation/ShopItemActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import com.myhome.android.shoppinglist.R
 
 class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ShopIteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+        viewModel = ShopIteViewModel()
+        viewModel.errorInputName
     }
 }
